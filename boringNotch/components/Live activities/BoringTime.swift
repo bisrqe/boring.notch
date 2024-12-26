9b8e6a0b6583c8c0a76308eb6263995f714c037e
Bismarck Animas Roque <bisanimroque@gmail.com>
2024-12-25 22:44:26 -0600

    Added a display for the current time. It shows it on the BoringHeader between the battery and settings icon (reordered those too). Additionally, added the toggle button inside the Appereance section in Settings under the "Additional Features" label.

---
+++ boringNotch/components/Live activities/BoringTime.swift
@@ -0,0 +1,18 @@
+//
+//  BoringTime.swift
+//  boringNotch
+//
+//  Created by Bismarck Animas Roque on 25/12/24.
+//
+
+import SwiftUI
+
+struct BoringTime: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BoringTime()
+}
